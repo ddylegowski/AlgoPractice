e37a053b74dea9d2eeabebf7025bd04edb8a293c
ddylegowski <david.dylegowski@gmail.com>
2014-11-04 08:58:31 -0600

    Adding StringToInteger

---
+++ src/com/ddylegowski/algopractice/StringToInteger.groovy
@@ -0,0 +1,53 @@
+package com.ddylegowski.algopractice
+
+/**
+ * StringToInteger solves the problem:
+ * Given an integer as a string, return the value as an integer.
+ *
+ */
+class StringToInteger {
+
+	//error messages for ArithmeticException
+	public static final String INVALID_CHAR = "Character is not a numerical digit";
+	public static final String EMPTY_INPUT = "Cannot convert an empty string to an integer";
+	
+	/**
+	 * Return the int value represented by the string.
+	 * The time complexity of this algorithm is O(N)
+	 * @param string
+	 * @return
+	 */
+	public static int stringToInteger(String string) {
+		
+		if (string.isEmpty()) {
+			throw new ArithmeticException(EMPTY_INPUT);
+		}
+		
+		int value = 0;
+		boolean negative = string.charAt(0) == '-';
+		for (int i = (negative ? 1 : 0); i < string.size(); ++i) {
+			value = (value*10) + charToInt(string.charAt(i));
+		}
+		
+		if (negative == true) {
+			value = value*-1;
+		}
+		
+		return value;
+	}
+	
+	/**
+	 * Given an int as a char, return the int value
+	 * @param c
+	 * @return
+	 */
+	private static int charToInt(char c) {
+		int ascii = (int)c;
+		if (ascii < 48 || ascii > 57) {
+			throw new ArithmeticException(INVALID_CHAR);
+		}
+		
+		return ascii - 48;
+	}
+	
+}
---
+++ test/com/ddylegowski/algopractice/StringToIntegerTest.groovy
@@ -0,0 +1,49 @@
+package com.ddylegowski.algopractice;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+class StringToIntegerTest {
+
+	final shouldFail = new GroovyTestCase().&shouldFail
+
+	@Test
+	public void test_stringToInteger_singleDigit() {
+		assertEquals(5, StringToInteger.stringToInteger("5"));
+	}
+	
+	@Test
+	public void test_stringToInteger_singleDigit_negative() {
+		assertEquals(-5, StringToInteger.stringToInteger("-5"));
+	}
+	
+	@Test
+	public void test_stringToInteger_zero() {
+		assertEquals(0, StringToInteger.stringToInteger("0"));
+	}
+	
+	@Test
+	public void test_stringToInteger() {
+		assertEquals(999, StringToInteger.stringToInteger("999"));
+	}
+	
+	@Test
+	public void test_stringToInteger_empty() {
+		shouldFail(ArithmeticException) {
+			int value = StringToInteger.stringToInteger("")
+		}
+	}
+	
+	@Test
+	public void test_stringToInteger_nonNumericString() {
+		shouldFail(ArithmeticException) {
+			int value = StringToInteger.stringToInteger("abc")
+		}
+	}
+
+}
